1cbbf1c8f2f773241ae993d4a9113184dc1d2e28
Koursi Bachar <koursibdbachar@yahoo.com>
2022-08-31 12:07:58 +0200

    test commit

---
+++ Ethereum.py
@@ -0,0 +1,97 @@
+import datetime
+from datetime import date
+import time
+import json
+import requests
+from timeit import default_timer as timer
+
+mstart = timer() #MSTART MASTER START | MEND MASTER END | MTIME MASTER TIME
+
+buyprice = 0.0
+sellprice = 0.0
+old_sellprice = 0.0
+total_profit = 0.0
+old_profit = 0.0
+w = 1 #W means wait
+
+key = "https://api.binance.com/api/v3/ticker/price?symbol="
+currencies = ["ETHUSDT"] #MAYBE ADD BTC SOON
+
+while 1: ###
+	try: ###
+		while 1:
+			#BUY
+			url = key+currencies[0]  
+			data = requests.get(url)
+			data = data.json()
+			buyprice = float(f"{data['price']}")
+			start = timer()
+			time.sleep(5) #OLD VALUE 15
+			url = key+currencies[0]  
+			data = requests.get(url)
+			data = data.json()
+			cprice1 = float(f"{data['price']}") #no sell price because didn't sell yet | cprice = check price
+			end = timer()
+			rise = cprice1 - buyprice
+			run = end - start
+			if rise/run > -0.05:
+				slope = rise/run
+				print(slope)
+				print('slope > -0.05\n')
+				continue
+			else:
+				###
+				while w == 1:
+					url = key+currencies[0]  
+					data = requests.get(url)
+					data = data.json()
+					fc1 = float(f"{data['price']}") #FINAL CHECK 1
+					time.sleep(10) ###TEST THIS NUMBER HEAVILY (ei 15)
+					url = key+currencies[0]  
+					data = requests.get(url)
+					data = data.json()
+					fc2 = float(f"{data['price']}") #FINAL CHECK 2
+					if fc1 > fc2 and cprice1 > fc2: #ALSO TRY FC1
+						w = 1
+					else:
+						w = 0
+				###
+				buyprice = float(f"{data['price']}") #THIS SIMULATES BUYING
+				print('Bought at: %f' %buyprice)
+				start1 = timer() ###
+				execute = 1
+			while 1:
+				#SELL
+				url = key+currencies[0]
+				data = requests.get(url) #
+				data = data.json()
+				sellprice = float(f"{data['price']}")
+				print(sellprice)
+				if execute == 1: ###
+					end1 = timer() ###
+					ttime = end1 - start1 ###
+					print('Transition time is %f' %ttime) ###
+					execute = 0 ###
+				else: ###
+					pass ###
+				if sellprice >= buyprice and sellprice - buyprice >= 0.25: #OR 0.5
+					url = key+currencies[0]  
+					data = requests.get(url)
+					data = data.json()
+					sellprice = float(f"{data['price']}") #THIS SIMULATES SELLING
+					profit = sellprice - buyprice
+					print('\nSOLD')
+					print('Profit: %f' %profit)
+					total_profit = profit + old_profit
+					print('Total profit: %f' %total_profit)
+					#
+					mend = timer()
+					mtime = mend - mstart
+					print('Elapsed time: %f seconds\n' %mtime)
+					#
+					old_profit = total_profit
+					break
+				else:
+					pass
+	except ConnectionResetError: ###
+		continue ###
